15dc7ca60bf733bcac44ce2995c3ce3ac31d462c
Ronaldo Carvalho <ronaldocarvalho798@gmail.com>
2025-06-12 13:27:18 -0300

    animation menu links click

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+console.log('ola')
+
+const menulinks = document.querySelectorAll('.itens-nav');
+
+
+ menulinks.forEach(item => {
+    item.addEventListener("click", () => {
+      menulinks.forEach(i => i.classList.remove("bg-rosa"));
+      item.classList.add("bg-rosa");
+    })
+  })
